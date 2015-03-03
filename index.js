bba453196d29d4f704b80341ef98c87721a30f10
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2015-03-03 15:43:45 +1300

    Add varidac object property args

--- index.js
+++ index.js
@@ -8,16 +8,25 @@
 module.exports.register = function (Handlebars, subresource) {
     subresource = subresource || require("subresource");
 
-    Handlebars.registerHelper("sri", function (filePath, property) {
+    // Varidac function
+    Handlebars.registerHelper("sri", function (filePath) {
         var resource = subresource(filePath);
 
-        // Default to integrity attribute
-        if (typeof property !== "string") {
-            property = "integrity";
+        // Default to integrity.
+        // Handlebars will attach an object as the last argument - ignore it!
+        if (arguments.length <= 2) {
+            return resource.integrity;
         }
 
-        // Static property
-        return resource[property];
+        // Grab an array of arguments, after filePath
+        var properties = [].slice.call(arguments, 1);
+        properties.pop(); // Ignore handlebars' argument
+
+        // Dig down through resource properties
+        // e.g. return resource[arg1][arg2][arg3][arg4];
+        return properties.reduce(function (child, property) {
+            return child[property];
+        }, resource);
     });
 
     return Handlebars;
