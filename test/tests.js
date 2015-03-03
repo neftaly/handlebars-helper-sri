952faa6d147fc815914a96384533e3c190c1b398
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2015-03-03 15:43:45 +1300

    Add varidac object property args

--- test/tests.js
+++ test/tests.js
@@ -1,22 +1,26 @@
 "use strict";
 
 var assert = require("assert"),
-    handlebars = require("handlebars"),
     fs = require("fs"),
+    handlebars = require("handlebars"),
 
-//    testString = "",
     filePath = "./test/jquery-1.10.2.min.js.testdata";
 
+// Attach handler
 handlebars = require("../index.js").register(handlebars);
 
-it("Default", function () {
-    var expect = "<script src='jquery.js' integrity='sha256-C6CB9UYIS9UJeqinPHWTHVqh/E1uhG5Twh+Y5qFQmYg='></script>";
-    var result = handlebars.compile("<script src='jquery.js' integrity='{{sri filePath}}'></script>")({ filePath: filePath });
-    assert.equal(expect, result);
-});
+describe("Standard subresource instance", function () {
 
-it.skip("Custom subresource instance", function () {
-    var expect = true;
-    var result = false;
-    assert.equal(expect, result);
+    it("Default", function () {
+        var expect = "<script src='jquery.js' integrity='sha256-C6CB9UYIS9UJeqinPHWTHVqh/E1uhG5Twh+Y5qFQmYg='></script>";
+        var result = handlebars.compile("<script src='jquery.js' integrity='{{sri filePath}}'></script>")({ filePath: filePath });
+        assert.equal(expect, result);
+    });
+
+    it("Property", function () {
+        var expect = "C6CB9UYIS9UJeqinPHWTHVqh/E1uhG5Twh+Y5qFQmYg=";
+        var result = handlebars.compile("{{sri filePath 'hashes' 'sha256'}}")({ filePath: filePath });
+        assert.equal(expect, result);
+    });
+
 });
